deb2a3dcabbcec2e982d720c07f9759a206fff45
Dhruv Varshney <47208019+dvdhruv@users.noreply.github.com>
2021-10-28 18:49:44 +0530

    Add files via upload

---
+++ hack1.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+void main()
+{
+    int N,T,a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i,j,k=0,l=0,m,n,o,p,q,r,s,t,u,v,w,x,z,y,count=0;
+
+    scanf("%d",&N);
+
+    while(N>0)
+    {
+        N=N/10;
+        count++;
+    }
+    while(N>0)
+    {
+        x=N%10;
+        y=y*10+x;
+        N=N/10;
+        if(y==0)
+    a++;
+    if(y==1)
+    b++;
+    if(y==2)
+    c++;
+    if(y==3)
+    d++;
+    if(y==4)
+    e++;
+    if(y==5)
+    f++;
+    if(y==6)
+    g++;
+    if(y==7)
+    h++;
+    if(y==8)
+    k++;
+    if(y==9)
+    l++;
+    }
+
+    m=1;
+    n=1;
+    o=1;
+    p=1;
+    q=1;
+    r=1;
+    s=1;
+    t=1;
+    u=1;
+    v=1;
+    for(i=1;i<=a;i++)
+    m=m*i;
+    for(i=1;i<=b;i++)
+    n=n*i;
+    for(i=1;i<=c;i++)
+    o=o*i;
+    for(i=1;i<=d;i++)
+    p=p*i;
+    for(i=1;i<=e;i++)
+    q=q*i;
+    for(i=1;i<=f;i++)
+    r=r*i;
+    for(i=1;i<=g;i++)
+    s=s*i;
+    for(i=1;i<=h;i++)
+    t=t*i;
+    for(i=1;i<=k;i++)
+    u=u*i;
+    for(i=1;i<=l;i++)
+    v=v*i;
+    w=m*n*o*p*q*r*s*t*u*v;
+    z=1;
+    for(i=1;i<=count;i++)
+    z=z*i;
+    T=z/w;
+    printf("%d",T);
+
+}
