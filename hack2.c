07f16da448c19bc617b6c25e0ed99610254a4ceb
Dhruv Varshney <47208019+dvdhruv@users.noreply.github.com>
2022-10-02 18:07:11 +0530

    Update hack2.c
    anything

--- hack2.c
+++ hack2.c
@@ -47,3 +47,20 @@ return 0;
 //   cout << "Hello World!";
 //   return 0;
 // }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
