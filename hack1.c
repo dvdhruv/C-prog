c5cbb19193a54f9856be8a214ffe09b7b52b1817
aviral2611 <69722104+aviral2611@users.noreply.github.com>
2022-10-02 18:04:40 +0530

    Update hack1.c
    anythiung

--- hack1.c
+++ hack1.c
@@ -94,4 +94,19 @@ void main()
 //   return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
 
