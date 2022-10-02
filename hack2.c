b27880839272e7d986369cc65616e7c756fda0d2
aviral2611 <69722104+aviral2611@users.noreply.github.com>
2022-10-02 18:08:40 +0530

    Update hack2.c
    anything

--- hack2.c
+++ hack2.c
@@ -64,3 +64,18 @@ return 0;
 //   cout << "Hello World!";
 //   return 0;
 // }
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
