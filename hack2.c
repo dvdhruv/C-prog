85429940900729eb2ae58d077e96c21f11d08841
BlackHatop <115545039+BlackHatop@users.noreply.github.com>
2022-10-11 15:59:42 +0530

    Update hack2.c

--- hack2.c
+++ hack2.c
@@ -79,3 +79,11 @@ return 0;
 //   cout << "Hello World!";
 //   return 0;
 // }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
