a4e78e8f6b2875e954ac055f3eb997c2ec4f87a0
aviral2600 <115558000+aviral2600@users.noreply.github.com>
2022-10-11 18:18:24 +0530

    Update hack2.c

--- hack2.c
+++ hack2.c
@@ -96,3 +96,11 @@ return 0;
 //   cout << "Hello World!";
 //   return 0;
 // }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
