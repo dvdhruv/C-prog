dc92abda55547bb34b6155eebe4a9db53bd0f579
BlackHatop <115545039+BlackHatop@users.noreply.github.com>
2022-10-11 16:00:22 +0530

    Update hack3.c

--- hack3.c
+++ hack3.c
@@ -49,3 +49,11 @@ void main()
 //   cout << "Hello World!";
 //   return 0;
 // }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
