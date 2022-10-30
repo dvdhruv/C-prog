ca6600f548a6778c162f18055c42f00b95f6beb7
DewanshGaur420 <117035166+DewanshGaur420@users.noreply.github.com>
2022-10-30 23:35:55 +0530

    Update hack4.c

--- hack4.c
+++ hack4.c
@@ -91,3 +91,19 @@ void main()
 //   cout << "Hello World!";
 //   return 0;
 // }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
