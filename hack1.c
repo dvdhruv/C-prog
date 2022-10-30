d0ef6bb3f9552b4b18590dcddc9553fbd14c93da
Abhishek Kapoor <42829419+Abhishek9111@users.noreply.github.com>
2022-10-30 23:02:31 +0530

    proposed changes

--- hack1.c
+++ hack1.c
@@ -134,3 +134,15 @@ void main()
 //   cout << "Hello World!";
 //   return 0;
 // }
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
